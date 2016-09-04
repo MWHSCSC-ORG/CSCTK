ecd2aeb4995bd257ea86e7f4e886fbae54ffd5eb
Aaron Vigal <admin@aaronvigal.com>
2016-09-04 17:12:37 -0500

    Update FindDeauth.py

--- FindDeauth.py
+++ FindDeauth.py
@@ -1,10 +1,10 @@
 import subprocess, sys, os
 # Start to find the BSSID of the network according to it's name
-f = open('/usr/share/aaronstoolkit/DEAUTHOutput.txt', 'r+')
+f = open('/usr/share/csctk/DEAUTHOutput.txt', 'r+')
 lines=f.readlines()
 line = lines[2]
 array = line.split(",")
 deauth = array[5].replace(" ", "")
 f.close()
-g = open('/usr/share/aaronstoolkit/deauth.txt', 'w')
+g = open('/usr/share/csctk/deauth.txt', 'w')
 g.write(deauth)
