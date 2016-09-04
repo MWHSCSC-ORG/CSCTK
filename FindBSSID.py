dfcbbdf814eb681d095d58c61540986781161eae
Aaron Vigal <admin@aaronvigal.com>
2016-09-04 17:12:21 -0500

    Update FindBSSID.py

--- FindBSSID.py
+++ FindBSSID.py
@@ -1,10 +1,10 @@
 import subprocess, sys, os
 # Get the name you entered in the bash script 
-with open('/usr/share/aaronstoolkit/name.txt', 'r') as myfile:
+with open('/usr/share/csctk/name.txt', 'r') as myfile:
     name=myfile.read().replace('\n', '')
 myfile.close()
 # Start to find the BSSID of the network according to it's name
-f = open('/usr/share/aaronstoolkit/BSSIDOutput.txt', 'r+')
+f = open('/usr/share/csctk/BSSIDOutput.txt', 'r+')
 lines=f.readlines()
 line = filter(lambda x: name in x, lines)
 string = ' '.join(line)
@@ -12,9 +12,9 @@ array = string.split(",")
 bssid = array[0].replace(" ", "")
 channel = array[3].replace(" ", "")
 f.close()
-g = open('/usr/share/aaronstoolkit/bssid.txt', 'w')
+g = open('/usr/share/csctk/bssid.txt', 'w')
 g.write(bssid)
 g.close()
-h = open('/usr/share/aaronstoolkit/channel.txt', 'w')
+h = open('/usr/share/csctk/channel.txt', 'w')
 h.write(channel)
 h.close()
